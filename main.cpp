7f710de0a6ac9704afb42476885e374269819451
Eldon <Birdheldon@gmail.com>
2025-02-22 23:42:17 -0700

    begin

---
+++ main.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+
+#include "cmake-build-debug/Audio.hpp"
+
+// TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+int main() {
+
+    Audio audio;
+
+
+
+
+    return 1;
+}
+
+// TIP See CLion help at <a
+// href="https://www.jetbrains.com/help/clion/">jetbrains.com/help/clion/</a>.
+//  Also, you can try interactive lessons for CLion by selecting
+//  'Help | Learn IDE Features' from the main menu.
